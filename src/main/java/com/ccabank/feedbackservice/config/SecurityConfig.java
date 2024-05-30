5d85c48df7e1d4e0c1e68164b6cf3d1845e28190
Simo Djoko Kevin <kevin.simo@cca-bank.com>
2024-05-30 11:46:30 +0100

    skip tests

--- src/main/java/com/ccabank/feedbackservice/config/SecurityConfig.java
+++ src/main/java/com/ccabank/feedbackservice/config/SecurityConfig.java
@@ -28,19 +28,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-         http .csrf().disable()
-                 .cors().configurationSource(corsConfigurationSource())
-                 .and()
-                 .authorizeRequests().anyRequest().permitAll();
+         http .csrf().disable();
     }
 
-    private CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("*"));
-        configuration.setAllowedMethods(Arrays.asList("*"));
-        configuration.setAllowedHeaders(Arrays.asList("*"));
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
 }
