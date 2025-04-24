b1dee326aced3730a2dca43c95adc7df12de7bcd
LONLA_G <gatien.lonla@cca-bank.com>
2025-04-24 13:07:07 +0100

    dependencies fixed

--- src/main/java/com/ccabank/paperless/openfeign/EntityRestClient.java
+++ src/main/java/com/ccabank/paperless/openfeign/EntityRestClient.java
@@ -1,13 +1,13 @@
 package com.ccabank.paperless.openfeign;
 
+import com.ccabank.paperless.config.FeignClientConfiguration;
 import com.ccabank.paperless.dto.entity.AgencyInfo;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.cloud.openfeign.FeignClientProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
-@FeignClient(name = "ENTITY-SERVICE" , configuration = FeignClientProperties.FeignClientConfiguration.class)
+@FeignClient(name = "ENTITY-SERVICE" , configuration = FeignClientConfiguration.class)
 public interface EntityRestClient {
 
     @GetMapping(path = "api/entities/agencies/getAllAgencies")
--- src/main/java/com/ccabank/paperless/openfeign/UserRestClient.java
+++ src/main/java/com/ccabank/paperless/openfeign/UserRestClient.java
@@ -1,28 +1,22 @@
 package com.ccabank.paperless.openfeign;
 
+import com.ccabank.paperless.config.FeignClientConfiguration;
 import com.ccabank.paperless.dto.user.EmployeeInfo;
 import com.ccabank.paperless.dto.user.UserRestDto;
 import feign.Headers;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.cloud.openfeign.FeignClientProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 
 /**
  * @author : <a href="mailto:patrick.simo@cca-bank.com">Patrick SIMO</a>
- * @project : user-service
- * @Package : com.ccabank.userservice.openfeign
- * <p>
- * @date: 12/08/2023
- * @time: 13:51
- * <p>
  * Created with IntelliJ IDEA
  * To change this template use File | Settings | File Templates.
  */
 
 
-@FeignClient(name = "USER-SERVICE", configuration = FeignClientProperties.FeignClientConfiguration.class)
+@FeignClient(name = "USER-SERVICE", configuration = FeignClientConfiguration.class)
 //@FeignClient(value = "userRestClient", url = "https://developer.ccabank-app.com", configuration = FeignClientProperties.FeignClientConfiguration.class)
 public interface UserRestClient {
 
