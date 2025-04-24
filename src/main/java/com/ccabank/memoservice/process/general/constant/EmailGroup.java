cebe9f3b09e62c8e3873799d5386c04b325a065e
Simo Djoko Trésor <kevin.simo@cca-bank.com>
2025-04-24 10:40:17 +0100

    merge

--- src/main/java/com/ccabank/memoservice/process/general/constant/EmailGroup.java
+++ src/main/java/com/ccabank/memoservice/process/general/constant/EmailGroup.java
@@ -2,8 +2,8 @@ package com.ccabank.memoservice.process.general.constant;
 
 public class EmailGroup {
 
-    public static String EMAIL_HABILITATION = "habilitations@cca-bank.com";
+    public static String EMAIL_HABILITATION = "alain.liedji@cca-bank.com,kevin.simo@cca-bank.com";
 
-    public static String EMAIL_CAPITAL_HUMAIN = "merline.bona@cca-bank.com,charlotte.azeufouet@cca-bank.com,thierry.makondi@cca-bank.com";
+    public static String EMAIL_CAPITAL_HUMAIN = "alain.liedji@cca-bank.com,kevin.simo@cca-bank.com";
 
 }
