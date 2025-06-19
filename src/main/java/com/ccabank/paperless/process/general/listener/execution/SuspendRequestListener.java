c8ba2c93b7c0440a1a83229e5ce3bf6fdc8f0820
Simo Djoko Trésor <kevin.simo@cca-bank.com>
2025-06-19 15:43:12 +0100

    push

--- src/main/java/com/ccabank/paperless/process/general/listener/execution/SuspendRequestListener.java
+++ src/main/java/com/ccabank/paperless/process/general/listener/execution/SuspendRequestListener.java
@@ -1,27 +1,27 @@
-package com.ccabank.paperless.process.general.listener.task;
+package com.ccabank.paperless.process.general.listener.execution;
 
 import com.ccabank.paperless.entity.Request;
 import com.ccabank.paperless.entity.RequestStatus;
 import com.ccabank.paperless.repository.RequestRepository;
 import lombok.RequiredArgsConstructor;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.DelegateTask;
+import org.camunda.bpm.engine.delegate.ExecutionListener;
 import org.camunda.bpm.engine.delegate.TaskListener;
 import org.springframework.stereotype.Component;
 
 
 @Component
 @RequiredArgsConstructor
-public class SuspendRequestListener  implements TaskListener {
+public class SuspendRequestListener  implements ExecutionListener {
 
     private final RequestRepository requestRepository;
 
     @Override
-    public void notify(DelegateTask delegateTask) {
-
-        String processInstanceId = delegateTask.getProcessInstanceId();
+    public void notify(DelegateExecution delegateExecution) throws Exception {
+        String processInstanceId = delegateExecution.getProcessInstanceId();
         Request request = requestRepository.findByInstanceId(processInstanceId);
         request.setStatus(RequestStatus.SUSPENDED);
         requestRepository.save(request);
-
     }
 }
