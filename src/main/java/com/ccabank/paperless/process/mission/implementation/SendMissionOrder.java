976265afab10bbe4d7df13d5d633afd48cac581a
Simo Djoko Trésor <kevin.simo@cca-bank.com>
2025-07-10 16:05:57 +0100

    push

--- src/main/java/com/ccabank/paperless/process/mission/implementation/SendMissionOrder.java
+++ src/main/java/com/ccabank/paperless/process/mission/implementation/SendMissionOrder.java
@@ -145,6 +145,7 @@ public class SendMissionOrder implements JavaDelegate {
 
         String apbt_n2 = (String) delegateExecution.getVariable(ApprobationLevel.APPROBATION_N2);
 
+
         if(apbt_n2 != null){
 
             EmployeeInfo n2 =  userRestClient.getStaffByUsername(apbt_n2);
