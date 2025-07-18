f6b3d5898926c620678f308dcc3fe7b7b46620c6
Simo Djoko Trésor <kevin.simo@cca-bank.com>
2025-07-18 17:04:32 +0100

    push

--- src/main/java/com/ccabank/paperless/process/absence/implementation/SendAbsence.java
+++ src/main/java/com/ccabank/paperless/process/absence/implementation/SendAbsence.java
@@ -230,6 +230,7 @@ public class SendAbsence implements JavaDelegate {
         ask.setSubject("Autorisation d'absence");
         ask.setbCC(Apbt_n1.getEmail() + "," + processUnityService.getEmailUnity(EmailGroup.EMAIL_CAPITAL_HUMAIN) + "," + processUnityService.getEmailUnity(EmailGroup.EMAIL_HABILITATION));
         AttachmentDto attachment = new AttachmentDto();
+
         attachment.setName("absence" + delegateExecution.getBusinessKey() + ".pdf");
         attachment.setData(Base64.getEncoder().encodeToString(resource.getByteArray()));
         ask.setAttachments(new AttachmentDto[]{attachment});
