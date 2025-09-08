129aaa3eadff8565402108b3537d3e4563a7c2d6
Kevin Simo <kevin.simo@cca-bank.com>
2025-09-08 09:48:36 +0100

    push

--- src/main/java/com/ccabank/paperless/process/general/listener/AssignmentTaskListener.java
+++ src/main/java/com/ccabank/paperless/process/general/listener/AssignmentTaskListener.java
@@ -6,6 +6,7 @@ import com.ccabank.paperless.entity.Request;
 import com.ccabank.paperless.repository.ApprobationRepository;
 import com.ccabank.paperless.repository.RequestRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.camunda.bpm.engine.delegate.DelegateTask;
 import org.camunda.bpm.engine.delegate.TaskListener;
 import org.springframework.stereotype.Component;
@@ -14,6 +15,7 @@ import java.util.List;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class AssignmentTaskListener implements TaskListener {
 
 
@@ -29,9 +31,14 @@ public class AssignmentTaskListener implements TaskListener {
         System.out.println("Assignation de la tâche à " + userTaskId);
 
         Request request = requestRepository.findByInstanceId(delegateTask.getProcessInstanceId());
+        log.info(request.getReference());
+        log.info(userTaskId);
 
         List<Approbation> approbations = approbationRepository.findByReferenceAndStaffAndStatus(request.getReference(), userTaskId, ApprovalStatus.ACCEPTED);
-        if (approbations.size() > 0) {
+        log.info(String.valueOf(approbations.size()));
+
+        if (!approbations.isEmpty()) {
+            log.info("complete task");
             delegateTask.setVariable("decision", true);
             delegateTask.complete();
         }
