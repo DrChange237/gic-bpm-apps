6ead3dd4a902682c36b6a9d0ef81b2d607040731
Kevin Simo <kevin.simo@cca-bank.com>
2025-09-23 16:42:03 +0100

    push

--- src/main/java/com/ccabank/paperless/process/general/listener/task/NoDoubleValidationListener.java
+++ src/main/java/com/ccabank/paperless/process/general/listener/task/NoDoubleValidationListener.java
@@ -1,5 +1,4 @@
-package com.ccabank.paperless.process.general.listener.execution;
-
+package com.ccabank.paperless.process.general.listener.task;
 
 import com.ccabank.paperless.entity.Approbation;
 import com.ccabank.paperless.entity.ApprovalStatus;
@@ -10,35 +9,37 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.camunda.bpm.engine.ProcessEngine;
 import org.camunda.bpm.engine.ProcessEngines;
-import org.camunda.bpm.engine.delegate.DelegateExecution;
-import org.camunda.bpm.engine.delegate.ExecutionListener;
+import org.camunda.bpm.engine.delegate.DelegateTask;
+import org.camunda.bpm.engine.delegate.TaskListener;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+
 @Component
 @RequiredArgsConstructor
 @Slf4j
-public class NoDoubleValidationListener implements ExecutionListener {
+public class NoDoubleValidationListener implements TaskListener {
 
-    private final ApprobationRepository approbationRepository;
+
     private final RequestRepository requestRepository;
+    private final ApprobationRepository approbationRepository;
 
     @Override
-    public void notify(DelegateExecution execution) throws Exception {
-        Request request = requestRepository.findByInstanceId(execution.getProcessInstanceId());
-        List<Approbation> approbations = approbationRepository.findByReferenceAndStaffAndStatus(request.getReference(), userTaskId, ApprovalStatus.ACCEPTED);
+    public void notify(DelegateTask delegateTask) {
+
+        Request request = requestRepository.findByInstanceId(delegateTask.getProcessInstanceId());
+        List<Approbation> approbations = approbationRepository.findByReferenceAndStaffAndStatus(request.getReference(), delegateTask.getAssignee() , ApprovalStatus.ACCEPTED);
         log.info(String.valueOf(approbations.size()));
 
         if (!approbations.isEmpty()) {
             ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
             processEngine.getTaskService()
                     .createTaskQuery()
-                    .processInstanceId(execution.getProcessInstanceId())
-                    .taskDefinitionKey(execution.getCurrentActivityId())
+                    .processInstanceId(delegateTask.getProcessInstanceId())
+                    .taskDefinitionKey(delegateTask.getTaskDefinitionKey())
                     .list()
                     .forEach(task -> processEngine.getTaskService().complete(task.getId()));
         }
-
     }
 }
