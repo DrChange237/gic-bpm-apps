3fb55b048e4d56e41ce13ca8d8bf7639159d3922
Kevin Simo <kevin.simo@cca-bank.com>
2025-10-02 07:26:42 +0100

    push

---
+++ src/main/java/com/ccabank/paperless/process/collect_signature/implementation/CSVerifyFileType.java
@@ -0,0 +1,29 @@
+package com.ccabank.paperless.process.collect_signature.implementation;
+
+import com.ccabank.paperless.exception.BadRequestException;
+import com.ccabank.paperless.openfeign.FileRestClient;
+import com.ccabank.paperless.service.faces.FileService;
+import com.ccabank.paperless.util.file.PdfUtils;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class CSVerifyFileType implements JavaDelegate {
+
+    private final FileRestClient fileRestClient;
+
+    @Override
+    public void execute(DelegateExecution delegateExecution) throws Exception {
+
+        String file = (String) delegateExecution.getVariable("file");
+        String fileBase = fileRestClient.getB64FileById(file);
+        if(!PdfUtils.isBase64Pdf(fileBase)) {
+            throw new BadRequestException("Le fichier doit ètre un pdf");
+        }
+    }
+}
--- src/main/java/com/ccabank/paperless/util/file/PdfUtils.java
+++ src/main/java/com/ccabank/paperless/util/file/PdfUtils.java
@@ -4,11 +4,33 @@ import com.itextpdf.text.*;
 import com.itextpdf.text.pdf.*;
 
 import java.io.*;
+import java.util.Base64;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PdfUtils {
+
+
+    public static boolean isBase64Pdf(String base64) {
+        try {
+            // Décoder la chaîne Base64
+            byte[] decodedBytes = Base64.getDecoder().decode(base64);
+
+            // Vérifier la signature PDF : "%PDF-" en ASCII
+            String header = new String(decodedBytes, 0, Math.min(decodedBytes.length, 5));
+            if (!header.startsWith("%PDF-")) {
+                return false;
+            }
+
+            // Vérifier si ça se termine par "%%EOF" (fin de fichier PDF)
+            String content = new String(decodedBytes);
+            return content.contains("%%EOF");
+        } catch (IllegalArgumentException e) {
+            // Erreur si la chaîne n’est pas du Base64 valide
+            return false;
+        }
+    }
 
 
     /**
