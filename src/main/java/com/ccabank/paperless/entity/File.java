68582665b02061c1be3aa9b0419fb4aff424a37b
Kevin Simo <kevin.simo@cca-bank.com>
2025-12-15 09:16:20 +0100

    push

--- src/main/java/com/ccabank/paperless/entity/File.java
+++ src/main/java/com/ccabank/paperless/entity/File.java
@@ -1,12 +1,17 @@
 package com.ccabank.paperless.entity;
 
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "T_FILE")
+@Getter
+@Setter
 public class File extends Auditable {
 
     @Id
