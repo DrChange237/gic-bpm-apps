316380d36a9129e01bbab7f33d74ca7854781709
Kevin Simo <kevin.simo@cca-bank.com>
2025-12-15 08:32:48 +0100

    push

--- src/main/java/com/ccabank/paperless/dto/memo/ChoiceDto.java
+++ src/main/java/com/ccabank/paperless/dto/memo/ChoiceDto.java
@@ -7,6 +7,8 @@ import java.io.Serializable;
 @Data
 public class ChoiceDto  implements Serializable {
 
+    private static final long serialVersionUID = -6924825942271051013L;
+
     private String label;
     private Object value;
 
