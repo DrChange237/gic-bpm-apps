18208d4821edde5815aa52608057c7c3494f6ddb
Kevin Simo <kevin.simo@cca-bank.com>
2025-12-15 09:11:40 +0100

    push

--- src/main/java/com/ccabank/paperless/dto/memo/FileDto.java
+++ src/main/java/com/ccabank/paperless/dto/memo/FileDto.java
@@ -28,4 +28,84 @@ public class FileDto implements Serializable {
     private RequestDto request;
 
     private MultipartFile multipartFile;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public long getSize() {
+        return size;
+    }
+
+    public void setSize(long size) {
+        this.size = size;
+    }
+
+    public LocalDateTime getAddDate() {
+        return addDate;
+    }
+
+    public void setAddDate(LocalDateTime addDate) {
+        this.addDate = addDate;
+    }
+
+    public String getProject() {
+        return project;
+    }
+
+    public void setProject(String project) {
+        this.project = project;
+    }
+
+    public String getFile() {
+        return file;
+    }
+
+    public void setFile(String file) {
+        this.file = file;
+    }
+
+    public RequestDto getRequest() {
+        return request;
+    }
+
+    public void setRequest(RequestDto request) {
+        this.request = request;
+    }
+
+    public MultipartFile getMultipartFile() {
+        return multipartFile;
+    }
+
+    public void setMultipartFile(MultipartFile multipartFile) {
+        this.multipartFile = multipartFile;
+    }
 }
